31fc976f706599d2b728add733016294f6120a73
tharushaRukshan2002 <tharusharukshan79@gmail.com>
2022-02-17 14:42:27 +0530

    change the btn

--- app/src/main/java/com/example/tut2/MainActivity.kt
+++ app/src/main/java/com/example/tut2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.tut2
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import android.os.Bundle
@@ -39,6 +40,9 @@ class MainActivity : AppCompatActivity() {
     //number of wrong answers
     private var numWrongAnswers: Int = 0
 
+    //intent for the 2nd view
+    private lateinit var activity2Intent: Intent
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
 
@@ -46,64 +50,74 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         accessingElements()
         createDogs()
-        randomImages()
+        randomImages()//displaying random images when the screen starts.
         val submitBtn: Button = findViewById(R.id.submitBtn)
         questionTxt = findViewById(R.id.questionTxt)
         val nextBtn: Button = findViewById(R.id.nextBtn)
-
+        val finishBtn: Button = findViewById(R.id.finishBtn)
 
         imageButton[0].setOnClickListener {
-            if (selectedAnswer == null) {
-                selectedAnswer = imageButton[0]
-            }
+           validation(imageButton[0])
 
         }
         imageButton[1].setOnClickListener {
-            if (selectedAnswer == null) {
-                selectedAnswer = imageButton[1]
-            }
+            validation(imageButton[1])
 
         }
         imageButton[2].setOnClickListener {
-            if (selectedAnswer == null) {
-                selectedAnswer = imageButton[2]
-            }
+            validation(imageButton[2])
 
         }
         nextBtn.setOnClickListener {
-            selectedAnswer = null
-            questionTxt.text = "Select the image for dog breed"
-            randomImages()
-        }
-
-
-
-        submitBtn.setOnClickListener {
-
-            randomBreed.setTypeface(null, Typeface.ITALIC)
-            questionTxt.text = "Your answer is"
-            when (selectedAnswer) {
-                correctAnswer -> {
-                    randomBreed.text = "CORRECT!"
-                    randomBreed.setTextColor((Color.parseColor("#0af531")))//green
-                    numCorrectAnswers += 1
-                }
-                null -> {
-                    questionTxt.text = "Guess!!!!"
-                }
-                else -> {
-                    randomBreed.text = "WRONG!"
-                    randomBreed.setTextColor((Color.parseColor("#e81e1e")))///red
-                    numWrongAnswers += 1
-                }
+            var i = 0
+            while (i < imageButton.size){
+                imageButton[i].isEnabled = true
+                i++
+            }
+            if (dogs.size != dogImagesNumber.size) {
+                submitBtn.isEnabled = false
+                selectedAnswer = null
+                questionTxt.text = "Select the image for dog breed"
+                randomImages()
+            } else {
+                nextBtn.isEnabled = false
             }
 
         }
+
+//        submitBtn.setOnClickListener {
+//            submitBtn.isEnabled = false
+//            randomBreed.setTypeface(null, Typeface.ITALIC)
+//            questionTxt.text = "Your answer is"
+//            when (selectedAnswer) {
+//                correctAnswer -> {
+//
+//                }
+//                null -> {
+//                    questionTxt.text = "Guess!!!!"
+//                }
+//                else -> {
+//                    randomBreed.text = "WRONG!"
+//                    randomBreed.setTextColor((Color.parseColor("#e81e1e")))///red
+//                    numWrongAnswers++
+//                }
+//            }
+//        }
+
+        finishBtn.setOnClickListener {
+            val correctStr: String = numCorrectAnswers.toString()
+            val wrongStr: String = numWrongAnswers.toString()
+            activity2Intent = Intent(this, MainActivity2::class.java)
+            activity2Intent.putExtra("correct_answers", correctStr)
+            activity2Intent.putExtra("wrong_answers", wrongStr)
+            startActivity(activity2Intent)
+        }
+
 
     }
 
     /**
-     * creates six dog objects and assassin it to dogs array
+     * creates 9 dog objects and assassin it to dogs array
      */
     private fun createDogs() {
         dogs.add(Dog(R.drawable.affenpinscher, "Affenpinscher"))
@@ -112,6 +126,9 @@ class MainActivity : AppCompatActivity() {
         dogs.add(Dog(R.drawable.akita, "Akita"))
         dogs.add(Dog(R.drawable.alaskankleekai, "Alaskan klee kai"))
         dogs.add(Dog(R.drawable.alaskanmalamute, "Alaskan Malamute"))
+        dogs.add(Dog(R.drawable.pomeranian, "Pomeranian"))
+        dogs.add(Dog(R.drawable.siberianhusky, "Siberian Husky"))
+        dogs.add(Dog(R.drawable.yellowlabradorretriever, "Labrador"))
 
     }
 
@@ -130,39 +147,58 @@ class MainActivity : AppCompatActivity() {
      * This will put random images into random image buttons. and the local array will keep track
      * of the numbers.
      */
-    @SuppressLint("SetTextI18n")
     private fun randomImages() {
+        val imageFrameNum = mutableListOf<Int>()
+        var i = 0
+        while (i < imageButton.size) {
+            val frameNum =
+                (0 until imageButton.size).random() //this number selects the ImageButton from the array
+            val dogPic = (0 until dogs.size).random() //random number to take dogs array dog
 
-        if (dogs.size != dogImagesNumber.size) {
+            if (frameNum !in imageFrameNum && dogPic !in dogImagesNumber) {
 
-            val imageFrameNum = mutableListOf<Int>()
-            var i = 0
-            while (i < imageButton.size) {
-                val frameNum =
-                    (0 until imageButton.size).random() //this number selects the ImageButton from the array
-                val dogPic = (0 until dogs.size).random() //random number to take dogs array dog
-
-                if (frameNum !in imageFrameNum && dogPic !in dogImagesNumber) {
-
-                    if (i == 0) {
-                        imageButton[frameNum].setImageResource(dogs[dogPic].image)// setting the image
-                        imageFrameNum.add(frameNum)
-                        correctAnswer = imageButton[frameNum]
-                        dogImagesNumber.add(dogPic)
-                        randomBreed.text = dogs[dogPic].name
-                        randomBreed.setTextColor((Color.parseColor("#FF5252")))
-                        ++i
-                    } else {
-                        imageButton[frameNum].setImageResource(dogs[dogPic].image)
-                        imageFrameNum.add(frameNum)
-                        dogImagesNumber.add(dogPic)
-                        ++i
-                    }
-
+                if (i == 0) {
+                    imageButton[frameNum].setImageResource(dogs[dogPic].image)// setting the image
+                    imageFrameNum.add(frameNum)
+                    correctAnswer = imageButton[frameNum]
+                    dogImagesNumber.add(dogPic)
+                    randomBreed.text = dogs[dogPic].name
+                    randomBreed.setTextColor((Color.parseColor("#FF5252")))
+                    ++i
+                } else {
+                    imageButton[frameNum].setImageResource(dogs[dogPic].image)
+                    imageFrameNum.add(frameNum)
+                    dogImagesNumber.add(dogPic)
+                    ++i
                 }
+
+
             }
         }
 
+
+    }
+
+    /**
+     * will validate the answer and disable all the buttons.
+     * @param button corresponding image button as the param
+     */
+    @SuppressLint("SetTextI18n")
+    private fun validation(button: ImageButton){
+        var i = 0
+        while (i < imageButton.size){
+            imageButton[i].isEnabled = false;
+            i++
+        }
+        if(button == correctAnswer){
+            randomBreed.text = "CORRECT!"
+            randomBreed.setTextColor((Color.parseColor("#0af531")))//green
+            numCorrectAnswers++
+        }else{
+            randomBreed.text = "WRONG!"
+            randomBreed.setTextColor((Color.parseColor("#e81e1e")))///red
+            numWrongAnswers++
+        }
     }
 
 
--- app/src/main/java/com/example/tut2/MainActivity2.kt
+++ app/src/main/java/com/example/tut2/MainActivity2.kt
@@ -1,11 +1,24 @@
 package com.example.tut2
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val correctTxt: TextView = findViewById(R.id.correctTxt)
+        val correctNum = intent.getStringExtra("correct_answers")
+        correctTxt.text = "$correctNum"
+
+        val wrongTxt: TextView = findViewById(R.id.wrongTxt)
+        val wrongNum = intent.getStringExtra("wrong_answers")
+        wrongTxt.text = "$wrongNum"
     }
 }
+
+
+
